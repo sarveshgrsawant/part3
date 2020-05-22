21538784026070915ef2706423b517720994328e
Sarvesh Sawant <sarvesh_s@outlook.com>
2020-05-23 03:07:32 +0530

    uibuild

---
+++ README.md
@@ -0,0 +1,1 @@
+https://person-backend06.herokuapp.com/
---
+++ build/precache-manifest.f5ddeff185c555b563db83b9768b7f7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38be957733d7b3269bbf96d33413b0f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1026e2f14ecb297021b",
+    "url": "/static/js/2.f32f3424.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f32f3424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d7641ed16fb8afb26d9",
+    "url": "/static/js/main.c57f6604.chunk.js"
+  },
+  {
+    "revision": "6c6880a3ce1203faed53",
+    "url": "/static/js/runtime-main.730eb1cb.js"
+  }
+]);
